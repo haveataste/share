cf3da76a77913dac2750f081616b62dfae38884e
athome <35514901+haveataste@users.noreply.github.com>
2025-03-29 18:10:14 +0800

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,11 +1,13 @@
-if [[ $1 =~ 'tokyomotion' ]];then
-curl -e 'https://www.tokyomotion.net/' -O $1
-elif [[ $1 =~ 'seg' ]];then
-curl -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36' -e 'https://avgle.com/' -O $1
-ls | grep .ts | sort -n -k1.5 | awk 'BEGIN{s="concat:";}{s=s$0"|";}END{cmd = "ffmpeg -i \""s"\" -c copy hls.mp4";sub(/\|\"/,"\"",cmd);print cmd;system(cmd);}'
-rm -rf *.ts
-elif [[ $1 =~ 'voyeurhit' ]];then
-curl -sSL -e 'https://member.voyeurhit.com/' -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36' -O $1
-else
-echo 'nolink'
+if [ $# -ne 1 ]; then
+  echo 'nolink'
+elif [[ $1 =~ 'tokyomotion' ]]; then
+  sudo curl -sSL -e 'https://www.tokyomotion.net/' -O $1
+elif [[ $1 =~ 'seg' ]]; then
+  curl -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36' -e 'https://avgle.com/' -O $1
+  ls | grep .ts | sort -n -k1.5 | awk 'BEGIN{s="concat:";}{s=s$0"|";}END{cmd = "ffmpeg -i \""s"\" -c copy hls.mp4";sub(/\|\"/,"\"",cmd);print cmd;system(cmd);}'
+  rm -rf *.ts
+elif [[ $1 =~ 'voyeurhit' ]]; then
+  curl -sSL -e 'https://member.voyeurhit.com/' -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36' -O $1
+elif [[ $1 =~ 'mp4' ]]; then
+  curl -O $1
 fi
